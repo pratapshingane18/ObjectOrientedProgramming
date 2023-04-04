347b701ee36d226b5ada4325e3c14cc0ddf0fa99
pratapshingane18 <sushrutrao465@gmail.com>
2023-04-05 01:31:27 +0530

    changes

---
+++ dynamicObj.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class Index{
+
+    public:
+    int val;
+    char c;
+
+    void print(){
+        cout<<c<<endl;
+    }
+
+    // getter for int
+    int getVal(){
+        return val;
+    } 
+
+    // getter for char
+    char getC(){
+        return c;
+    }
+
+    // Setter int
+    void setVal(int x){
+        val = x;
+    }
+
+    // Setter val
+    void setC(char y){
+        c = y;
+    }
+
+
+
+};
+
+int main(){
+
+    // Dynamic Memory Allocation
+
+    Index *I = new Index;
+    I->setVal(10);
+    I->setC('P');
+
+    cout<<"Val is: "<<I->getVal()<<endl;
+    cout<<"Char is: "<<I->getC()<<endl;
+    
+    return 0;
+}
