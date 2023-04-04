bb50067b4949dea2bf4ef02e2bcbefbd8e5cc61f
pratapshingane18 <sushrutrao465@gmail.com>
2023-04-05 00:17:26 +0530

    intro program

---
+++ intro.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+// class 
+class Hero{
+
+    // Properties of a class
+    int val;
+};
+
+int main(){
+
+    //Object of a class
+    Hero prat;
+    //Printing size of Hero prat object
+    cout<<"Size of prat object: "<<sizeof(prat)<<endl;
+
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+// class 
+class Hero{
+
+    // Properties of a class
